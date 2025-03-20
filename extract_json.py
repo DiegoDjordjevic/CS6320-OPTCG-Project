2cefc0f3e7ec52f8e1a91011a0284019c2954e59
Diego Djordjevic <djordjevic.diego@gmail.com>
2025-03-20 02:00:07 -0500

    updated classes to store card rarity and card type. Removed error where leader had a cost attribute as well as a life attribute. Next Steps: Consider parsing multioption attributes into lits such as the color attribute.

--- extract_json.py
+++ extract_json.py
@@ -5,9 +5,10 @@ import json
 class Card:
     def __init__(self, card_id, rarity, card_type, card_set, name, attribute, power, counter, color, feature, text, trigger):
         self.card_id = card_id
+        self.rarity = rarity
+        self.card_type = card_type
         self.card_set = card_set
         self.name = name
-        self.cost = cost
         self.attribute = attribute
         self.power = power
         self.counter = counter
@@ -17,18 +18,18 @@ class Card:
         self.trigger = trigger
 
 class Leader(Card):
-    def __init__(self, card_id, card_set, name, life, attribute, power, counter, color, feature, text, trigger):
-        super().__init__(card_id, card_set, name, attribute, power, counter, color, feature, text, trigger)
+    def __init__(self, card_id, rarity, card_type, card_set, name, life, attribute, power, counter, color, feature, text, trigger):
+        super().__init__(card_id, rarity, card_type, card_set, name, attribute, power, counter, color, feature, text, trigger)
         self.life = life
 
 class Character(Card):
-    def __init__(self, card_id, card_set, name, cost, attribute, power, counter, color, feature, text, trigger):
-        super().__init__(card_id, card_set, name, attribute, power, counter, color, feature, text, trigger)
+    def __init__(self, card_id, rarity, card_type, card_set, name, cost, attribute, power, counter, color, feature, text, trigger):
+        super().__init__(card_id, rarity, card_type, card_set, name, attribute, power, counter, color, feature, text, trigger)
         self.cost = cost
 
 class Event(Card):
-    def __init__(self, card_id, card_set, name, cost, attribute, power, counter, color, feature, text, trigger):
-        super().__init__(card_id, card_set, name, attribute, power, counter, color, feature, text, trigger)
+    def __init__(self, card_id, rarity, card_type, card_set, name, cost, attribute, power, counter, color, feature, text, trigger):
+        super().__init__(card_id, rarity, card_type, card_set, name, attribute, power, counter, color, feature, text, trigger)
         self.cost = cost
 
 
@@ -56,7 +57,7 @@ for filename in os.listdir(cardlist_path):      #iterates over every file in the
                 rarity = info[1].strip()
                 card_type = info[2].strip()
                 info = [x.strip() for x in info]
-                print(info)
+                #print(info)
                 name = cardName.text
                 #removes Cost prefix for characters, events, and stages, as well as removes Life prefix for Leaders
                 cost = backCol.find('div', class_='cost').text.removeprefix('Cost').removeprefix('Life')
@@ -65,7 +66,7 @@ for filename in os.listdir(cardlist_path):      #iterates over every file in the
                 #removes Power prefix and leaves only the power value itself
                 power = backCol.find('div', class_='power').text.removeprefix('Power')
                 #removes Counter prefix and leaves only the counter value itself
-                counter = backCol.find('div', class_='counter').text.removeprefix('Counter')
+                counter = backCol.find('div', class_='counter').text.removeprefix('Counter').removesuffix('-')
                 #removes Color prefix and leaves only the color itself
                 color = backCol.find('div', class_='color').text.removeprefix('Color')
                 #removes Type prefix and leaves just the type itself
@@ -77,10 +78,19 @@ for filename in os.listdir(cardlist_path):      #iterates over every file in the
                 trigger = trigger.text.removeprefix('Trigger') if trigger else ''
                 #
                 card_set = backCol.find('div', class_='getInfo').text.removeprefix('Card Set(s)-')
-                print(card_set)
-                print(name)
-                print([cost, attribute, power, counter, color, feature, text, trigger])
-                print()
+                #print(card_set)
+                #print(name)
+                #print([cost, attribute, power, counter, color, feature, text, trigger])
+                #print()
+                match card_type:
+                    case 'LEADER':
+                        card = Leader(card_id, rarity, card_type, card_set, name, cost, attribute, power, counter, color, feature, text, trigger)
+                    case 'CHARACTER':
+                        card = Character(card_id, rarity, card_type, card_set, name, cost, attribute, power, counter, color, feature, text, trigger)
+                    case 'EVENT':
+                        card = Event(card_id, rarity, card_type, card_set, name, cost, attribute, power, counter, color, feature, text, trigger)
+
+                print(json.dumps(card.__dict__, indent=4))
                 break
             break
         #print(infoCols)
