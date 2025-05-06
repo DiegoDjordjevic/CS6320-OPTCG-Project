1667b9807fd4d182ccec66ae2e64eb2ec0304f68
Diego Djordjevic <djordjevic.diego@gmail.com>
2025-05-06 12:37:14 -0500

    Modified vector store creation to use langchain and google-genai embedding. Modified to add metadata to help filter cards based on their type. updated requirements.

--- build_vector_store.py
+++ build_vector_store.py
@@ -1,24 +1,35 @@
 import chromadb
-from pprint import pprint
 import json
+from langchain_chroma import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import os
+from dotenv import load_dotenv
+from langchain_core.documents import Document
 
-client = chromadb.Client()
 
-card_base = client.create_collection("OPTCG")
+load_dotenv()
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
+
+vector_store = Chroma(
+    collection_name="OPTCG",
+    embedding_function=embeddings,
+    persist_directory="./chroma_langchain_db",
+)
 
 f = open("asia-cardlist.json")
 data = json.load(f)
 documents = []
 ids = []
 for i in data:
-    documents.append(str(data[i]))
+    documents.append(Document(page_content=str(data[i]), metadata={"card_type": data[i]['card_type']}))
     ids.append(data[i]['card_id'])
 #print("documents" + str(documents))
+vector_store.add_documents(documents=documents, ids=ids)
 
-card_base.upsert(documents=documents, ids=ids)
-results = card_base.query(
-    query_texts=["Strawhat, Red, Leader"], # Chroma will embed this for you
-    n_results=5 # how many results to return
-)
 
-print(results)
+retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 5, "fetch_k": 25}, filter={"card_type": "LEADER"})
+retriever.invoke("Strawhat, Red, Leader")
+
+results = vector_store.similarity_search(query="Strawhat, Red, Leader",k=5,filter={"card_type": "LEADER"})
+for doc in results:
+    print(f"* {doc.page_content} [{doc.metadata}]")
