b32572fd3913403423d478655f8294b1be4de731
Diego Djordjevic <djordjevic.diego@gmail.com>
2025-05-06 23:13:18 -0500

    updated vectore store to store texts instead of Documents.

--- build_vector_store.py
+++ build_vector_store.py
@@ -4,8 +4,6 @@ from langchain_chroma import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import os
 from dotenv import load_dotenv
-from langchain_core.documents import Document
-
 
 load_dotenv()
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.getenv("GOOGLE_API_KEY"))
@@ -20,11 +18,12 @@ f = open("asia-cardlist.json")
 data = json.load(f)
 documents = []
 ids = []
+metadata = []
 for i in data:
-    documents.append(Document(page_content=str(data[i]), metadata={"card_type": data[i]['card_type']}))
+    documents.append(str(data[i]))
+    metadata.append({"card_type": data[i]['card_type']})
     ids.append(data[i]['card_id'])
-#print("documents" + str(documents))
-vector_store.add_documents(documents=documents, ids=ids)
+vector_store.add_texts(texts=documents, ids=ids, metadatas=metadata)
 
 
 retriever = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 5, "fetch_k": 25}, filter={"card_type": "LEADER"})
